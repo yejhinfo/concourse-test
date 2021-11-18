231b9c3b547ef5f1205fe91140679db17ad66954
ranyue <9378299+ranyue233@user.noreply.gitee.com>
2021-11-18 17:13:38 +0800

    test

--- hello.sh
+++ hello.sh
@@ -6,4 +6,15 @@ set -eu
 # Nominal range: 0 - 32767 (signed 16-bit integer).
 
 # This script will succeed (exit 0) 50% of the time
-echo "helo"
+value=$RANDOM
+if [[ $value -gt 16384 ]]; then
+  cat > notify_message/message <<EOF
+Unfortunately the \`test\` job failed. The random value $value needed to be less than 16384 to succeed.
+EOF
+  exit 1
+else
+  cat > notify_message/message <<EOF
+Hurray! The \`test\` job succeeded. The random value $value needed to be less than 16384 to succeed.
+EOF
+  exit 0
+fi
