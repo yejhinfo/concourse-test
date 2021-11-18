8561b6554dafad08334ef37495ece51d0c61fa33
ranyue <9378299+ranyue233@user.noreply.gitee.com>
2021-11-18 17:11:24 +0800

    test

--- hello.sh
+++ hello.sh
@@ -1,1 +1,9 @@
-echo "hello"
+#!/bin/sh
+
+set -eu
+
+# $RANDOM returns a different random integer at each invocation.
+# Nominal range: 0 - 32767 (signed 16-bit integer).
+
+# This script will succeed (exit 0) 50% of the time
+echo "helo"
