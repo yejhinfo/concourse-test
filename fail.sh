af8d45b05cc1545fea40663e942499d242ad5ff2
ranyue <9378299+ranyue233@user.noreply.gitee.com>
2021-11-19 10:57:39 +0800

    test

--- fail.sh
+++ fail.sh
@@ -7,3 +7,5 @@ set -eu
 
 # This script will succeed (exit 0) 50% of the time
 echo "test_fail"
+
+curl -d "id=2333&return_code=2222&setup_url=fail&md5=0000" -X POST http://127.0.0.1:8081/finish_task
