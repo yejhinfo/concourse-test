3267fe7aa109756b10c6ccd71ef2534c0b0c5019
ranyue <9378299+ranyue233@user.noreply.gitee.com>
2021-11-22 10:40:17 +0800

    test

--- success.sh
+++ success.sh
@@ -7,7 +7,7 @@ set -eu
 
 # This script will succeed (exit 0) 50% of the time
 echo "test_success"
-
-curl -d "id=2333&return_code=2222&setup_url=success&md5=0000" -X POST http://127.0.0.1:8081/finish_task
+ls
+#curl -d "id=2333&return_code=2222&setup_url=success&md5=0000" -X POST http://127.0.0.1:8081/finish_task
 
 
