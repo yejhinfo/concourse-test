3469be0ed5416381121a36d3754035dd22957bb4
ranyue <9378299+ranyue233@user.noreply.gitee.com>
2021-11-22 10:45:20 +0800

    test

--- success.sh
+++ success.sh
@@ -8,6 +8,7 @@ set -eu
 # This script will succeed (exit 0) 50% of the time
 echo "test_success"
 ls /
+apt-get update
 apt-get install curl
 #curl -d "id=2333&return_code=2222&setup_url=success&md5=0000" -X POST http://127.0.0.1:8081/finish_task
 
