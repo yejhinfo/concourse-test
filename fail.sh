dd1c78b99f781254ebd6b2f5b0612cfe0599e2f9
ranyue <9378299+ranyue233@user.noreply.gitee.com>
2021-11-22 10:43:20 +0800

    test

--- fail.sh
+++ fail.sh
@@ -8,5 +8,6 @@ set -eu
 # This script will succeed (exit 0) 50% of the time
 echo "test_fail"
 ls /
+apt-get install curl
 #curl -d "id=2333&return_code=2222&setup_url=fail&md5=0000" -X POST http://127.0.0.1:8081/finish_task
 
