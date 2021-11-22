1798e8b3e85d8ca178d6f8df7470d3c06742ec19
ranyue <9378299+ranyue233@user.noreply.gitee.com>
2021-11-22 17:25:15 +0800

    test

---
+++ status.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -eu
+
+# $RANDOM returns a different random integer at each invocation.
+# Nominal range: 0 - 32767 (signed 16-bit integer).
+
+# This script will succeed (exit 0) 50% of the time
+echo "test_status"
+tee /etc/apt/sources.list << EOF
+deb http://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ focal-security main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-security main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ focal-updates main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-updates main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ focal-proposed main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-proposed main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse
+EOF
+apt-get update
+apt-get -y install curl
+
+curl  http://10.0.2.15:8080/api/v1/teams/main/pipelines/tutorial-pipeline/jobs/test/builds -X GET -H "Authorization: bearer wFimuRjZsV3rlg13iSs0uuBjMY4MgnhjAAAAAA"
