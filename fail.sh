3ec7a602309d41c8f404338764d0cd17b877e0af
ranyue <9378299+ranyue233@user.noreply.gitee.com>
2021-11-24 11:02:11 +0800

    test

--- fail.sh
+++ fail.sh
@@ -21,6 +21,6 @@ deb-src http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted univer
 EOF
 apt-get update
 apt-get -y install curl
-curl -d "id=2333&return_code=2222&setup_url=fail&md5=0000" -X POST http://10.0.2.15:8082/finish_task
+curl -d "return_code=fail" -X POST http://10.0.2.15:8082/finish_task
 
 
